2d425c544f7fa18d3128c00e5bbb3f81e9793ef3
a-fei <xuchengfei_work@163.com>
2018-03-17 14:10:24 +0800

    新增签到的Controller+重新引用nuget包修改不能生成的问题

---
+++ FrameWork.Entity/ViewModel/SignIn/GetSignInInfoRequest.cs
@@ -0,0 +1,21 @@
+﻿using FrameWork.Common;
+using FrameWork.Entity.Entity;
+
+namespace FrameWork.Entity.ViewModel.SignIn
+{
+    /// <summary>
+    /// 参数
+    /// </summary>
+    public class GetSignInInfoRequest
+    {
+        /// <summary>
+        /// token
+        /// </summary>
+        public string Token { set; get; }
+
+        /// <summary>
+        /// 微信唯一标识符
+        /// </summary>
+        public string OpenId { set; get; }
+    }
+}
---
+++ XinDaPartJobAPI/XinDaPartJobAPI/Controllers/SignInController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using FrameWork.Common;
+using FrameWork.Entity.ViewModel.SignIn;
+using FrameWork.Web;
+
+namespace XinDaPartJobAPI.Controllers
+{
+    public class SignInController : AdminControllerBase
+    {
+        // GET: SignIn
+        public ActionResult Index(GetSignInInfoRequest request)
+        {
+            var userIndo = RedisInfoHelper.GetRedisModel(request.Token);
+            return null;
+        }
+    }
+}
